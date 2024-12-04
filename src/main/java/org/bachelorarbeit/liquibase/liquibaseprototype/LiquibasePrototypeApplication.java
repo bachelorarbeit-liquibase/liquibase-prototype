ba1d3c5d6c947f4ea9b7bc99ccb057df6c8132c2
Richard <straehle.richard@gmail.com>
2024-12-04 14:46:13 +0100

    init

---
+++ src/main/java/org/bachelorarbeit/liquibase/liquibaseprototype/LiquibasePrototypeApplication.java
@@ -0,0 +1,13 @@
+package org.bachelorarbeit.liquibase.liquibaseprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibasePrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibasePrototypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bachelorarbeit/liquibase/liquibaseprototype/LiquibasePrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bachelorarbeit.liquibase.liquibaseprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibasePrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
